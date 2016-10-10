d867b4caa06e40d1fa478ea69aa2bc332b9bf0ca
wyy20314 <838049784@qq.com>
2016-10-10 09:49:58 +0800

    create a new project

---
+++ PersonalDrame/PersonalDrame/Goal/Controller/GoalViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GoalViewController.swift
+//  PersonalDrame
+//
+//  Created by 水蒸气很快乐 on 16/10/9.
+//  Copyright © 2016年 水蒸气很快乐. All rights reserved.
+//
+
+import UIKit
+
+class GoalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
