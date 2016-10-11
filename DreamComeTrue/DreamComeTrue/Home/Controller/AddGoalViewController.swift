fa6a27f77414e18c92d067af752dc5f2dec4874c
wyy20314 <838049784@qq.com>
2016-10-11 12:38:49 +0800

    change home file

---
+++ DreamComeTrue/DreamComeTrue/Home/Controller/AddGoalViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddGoalViewController.swift
+//  DreamComeTrue
+//
+//  Created by Unite-iMac on 16/10/11.
+//  Copyright © 2016年 水蒸气很快乐. All rights reserved.
+//
+
+import UIKit
+
+class AddGoalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "创建目标"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DreamComeTrue/DreamComeTrue/Home/Controller/MainViewController.swift
+++ DreamComeTrue/DreamComeTrue/Home/Controller/MainViewController.swift
@@ -8,19 +8,62 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController,UIScrollViewDelegate {
+
+    //目标个数
+    @IBOutlet weak var lblGoalNum: UILabel!
+
+    //当前目标数
+    @IBOutlet weak var lblCurrentGoalNum: UILabel!
+
+    //总金额
+    @IBOutlet weak var lblTotalAmount: UILabel!
+
+    //共实现目标数
+    @IBOutlet weak var lblTotalGoalNum: UILabel!
+
+    //总计金额
+    @IBOutlet weak var lblAmount: UILabel!
+
+    //滚动视图
+    @IBOutlet weak var scrollview: UIScrollView!
+
+    //目标view
+    @IBOutlet weak var goalView: UIView!
+
+    //添加一个目标按钮
+    @IBOutlet weak var btnAddGoal: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //添加目标
+        btnAddGoal.addTarget(self, action:#selector(AddGoal_OnClick(_:)), for:.touchUpInside)
 
-        // Do any additional setup after loading the view.
+//        let imageView=UIImageView(image:UIImage(named:"creat_background"))
+//        
+//        scrollview.delegate = self
+//        scrollview.addSubview(imageView);
+//        self.view.addSubview(scrollview)
+        //判断一下目标的个数是否为零
+        //如果为零，显示添加新目标
+
+        //如果不为零，显示目标一
+
+
     }
-
+    func AddGoal_OnClick(_ button:UIButton){
+        //跳转到添加页面
+        let vc = AddGoalViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    //视图滚动中一直触发
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        print("x:\(scrollView.contentOffset.x) y:\(scrollView.contentOffset.y)")
+    }
 
     /*
     // MARK: - Navigation
