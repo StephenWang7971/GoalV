60fe438600494cf43cb61d077dc6e301ad1ebf36
wyy20314 <838049784@qq.com>
2016-10-11 13:55:25 +0800

    modify a problem

--- DreamComeTrue/DreamComeTrue/Home/Controller/AddGoalViewController.swift
+++ DreamComeTrue/DreamComeTrue/Home/Controller/AddGoalViewController.swift
@@ -7,20 +7,6 @@
 //
 
 
-import Foundation
-import CoreData
-extension Goal {
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<Goal> {
-        return NSFetchRequest<Goal>(entityName: "Goal");
-    }
-    @NSManaged public var address: String?
-    @NSManaged public var email: String?
-    @NSManaged public var headImg: NSData?
-    @NSManaged public var isLeave: Bool
-    @NSManaged public var name: String?
-    @NSManaged public var tel: String?
-    @NSManaged public var birthDay: String?
-}
 import UIKit
 class AddGoalViewController: UIViewController {
     //名称
@@ -64,49 +50,49 @@ class AddGoalViewController: UIViewController {
 
         //使用coredata来创建数据，进行保存
       
-        storePerson(name: "sfsdfaaa", starttime: "sfsdfbbb", finishtime: "sfsdfccc", account: "sfsdfddd")
-        getPerson()
+//        storePerson(name: "sfsdfaaa", starttime: "sfsdfbbb", finishtime: "sfsdfccc", account: "sfsdfddd")
+//        getPerson()
     }
 
     //获取Context，为了方便使用，这里封装成一个函数
-    func getContext () -> NSManagedObjectContext {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        return appDelegate.persistentContainer.viewContext
-    }
-    //存储数据
-    func storePerson(name:String, starttime:String,finishtime:String,account:String){
-        let context = getContext()
-        // 定义一个entity，这个entity一定要在xcdatamodeld中做好定义
-        let entity = NSEntityDescription.entity(forEntityName: "Goal", in: context)
-
-        let goal = NSManagedObject(entity: entity!, insertInto: context)
-
-        goal.setValue(name, forKey: "name")
-        goal.setValue(starttime, forKey: "starttime")
-        goal.setValue(finishtime, forKey: "finish")
-        goal.setValue(account, forKey: "account")
-
-        do {
-            try context.save()
-            print("saved")
-        }catch{
-            print(error)
-        }
-    }
-    // 获取某一entity的所有数据
-    func getPerson(){
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Goal")
-        do {
-            let searchResults = try getContext().fetch(fetchRequest)
-            print("numbers of \(searchResults.count)")
-
-            for p in (searchResults as! [NSManagedObject]){
-                print("name:  \(p.value(forKey: "name")!) starttime: \(p.value(forKey: "starttime")!)")
-            }
-        } catch  {
-            print(error)
-        }
-    }
+//    func getContext () -> NSManagedObjectContext {
+//        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//        return appDelegate.persistentContainer.viewContext
+//    }
+//    //存储数据
+//    func storePerson(name:String, starttime:String,finishtime:String,account:String){
+//        let context = getContext()
+//        // 定义一个entity，这个entity一定要在xcdatamodeld中做好定义
+//        let entity = NSEntityDescription.entity(forEntityName: "Goal", in: context)
+//
+//        let goal = NSManagedObject(entity: entity!, insertInto: context)
+//
+//        goal.setValue(name, forKey: "name")
+//        goal.setValue(starttime, forKey: "starttime")
+//        goal.setValue(finishtime, forKey: "finish")
+//        goal.setValue(account, forKey: "account")
+//
+//        do {
+//            try context.save()
+//            print("saved")
+//        }catch{
+//            print(error)
+//        }
+//    }
+//    // 获取某一entity的所有数据
+//    func getPerson(){
+//        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Goal")
+//        do {
+//            let searchResults = try getContext().fetch(fetchRequest)
+//            print("numbers of \(searchResults.count)")
+//
+//            for p in (searchResults as! [NSManagedObject]){
+//                print("name:  \(p.value(forKey: "name")!) starttime: \(p.value(forKey: "starttime")!)")
+//            }
+//        } catch  {
+//            print(error)
+//        }
+//    }
     /*
     // MARK: - Navigation
 
