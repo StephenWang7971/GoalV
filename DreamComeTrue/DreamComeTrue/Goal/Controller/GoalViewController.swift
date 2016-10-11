b9b27e45ca40c39670f779c662e41285f84d285b
shuizheng <m15734107984@163.com>
2016-10-11 16:04:45 +0800

    Create   GoalViewController’s xib

--- DreamComeTrue/DreamComeTrue/Goal/Controller/GoalViewController.swift
+++ DreamComeTrue/DreamComeTrue/Goal/Controller/GoalViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class GoalViewController: UIViewController
 {
     
+    //var  mainScrollView : UIScrollView?
+
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         setupRightBarButtonItem()
+       
     }
+    //创建导航栏右侧按钮
      func setupRightBarButtonItem()
      {
         let rightBtn: UIButton = UIButton(type: .custom)
@@ -30,17 +35,41 @@ class GoalViewController: UIViewController
         self.navigationItem.rightBarButtonItem = barButtonItem
 
      }
+    //创建目标按钮
      func setupGoalButton()
      {
         let FGBtn:UIButton = UIButton(type: .custom)
-        FGBtn.frame = CGRect(x: 0,y: 0,width: 40,height:25)
+        FGBtn.frame = CGRect(x: 0,y: 0,width:kMAIN_SIZE.width/4,height:kMAIN_SIZE.height/20)
         FGBtn.setTitle("上一目标", for: UIControlState())
-        FGBtn.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        FGBtn.titleLabel?.font = UIFont.systemFont(ofSize: 9)
         FGBtn.setTitleColor(UIColor.white, for: UIControlState())
-        FGBtn.layer.borderColor = UIColor.white.cgColor
-        FGBtn.layer.borderWidth = 1
-        FGBtn.layer.cornerRadius = 3
-     }
+        FGBtn.backgroundColor = UIColor.red
+        FGBtn.layer.borderWidth = 0.5
+        FGBtn.layer.cornerRadius = 2
+        let SGBtn:UIButton = UIButton(type: .custom)
+        SGBtn.frame = CGRect(x: 0,y: 0,width: 40,height:25)
+        SGBtn.setTitle("目标一", for: UIControlState())
+        SGBtn.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        SGBtn.setTitleColor(UIColor.white, for: UIControlState())
+        SGBtn.layer.borderColor = UIColor.white.cgColor
+        SGBtn.layer.borderWidth = 1
+        SGBtn.layer.cornerRadius = 3
+        let TGBtn:UIButton = UIButton(type: .custom)
+        TGBtn.frame = CGRect(x: 0,y: 0,width: 40,height:25)
+        TGBtn.setTitle("下一目标", for: UIControlState())
+        TGBtn.titleLabel?.font = UIFont.systemFont(ofSize:9)
+        TGBtn.setTitleColor(UIColor.white, for: UIControlState())
+        TGBtn.layer.borderColor = UIColor.white.cgColor
+        TGBtn.layer.borderWidth = 1
+        TGBtn.layer.cornerRadius = 3
+        self.view.addSubview(FGBtn)
+        
+    }
+    //创建全局滚动视图
+    func setupMainScrollView()
+    {
+    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
