db30551b0400769da2670381be49fc644c1d81a8
shuizheng <m15734107984@163.com>
2016-10-11 13:28:12 +0800

    Create GoalViewController’s     navgationbar rightButton

--- DreamComeTrue/DreamComeTrue/Goal/Controller/GoalViewController.swift
+++ DreamComeTrue/DreamComeTrue/Goal/Controller/GoalViewController.swift
@@ -1,21 +1,35 @@
 //
 //  GoalViewController.swift
-//  PersonalDrame
+//  DreamComeTrue
 //
-//  Created by 水蒸气很快乐 on 16/10/9.
-//  Copyright © 2016年 One Hydrogen. All rights reserved.
+//  Created by 水蒸气很快乐 on 16/10/11.
+//  Copyright © 2016年 水蒸气很快乐. All rights reserved.
 //
 
 import UIKit
 
-class GoalViewController: UIViewController {
+class GoalViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-         self.view.backgroundColor = UIColor.white
-        // Do any additional setup after loading the view.
+        setupRightBarButtonItem()
     }
+     func setupRightBarButtonItem()
+     {
+        let rightBtn: UIButton = UIButton(type: .custom)
+        rightBtn.frame = CGRect(x: 0,y: 0,width: 40,height:25)
+        rightBtn.setTitle("编辑", for: UIControlState())
+        rightBtn.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        rightBtn.setTitleColor(UIColor.white, for: UIControlState())
+        rightBtn.layer.borderColor = UIColor.white.cgColor
+        rightBtn.layer.borderWidth = 1
+        rightBtn.layer.cornerRadius = 3
+        let barButtonItem = UIBarButtonItem(customView:rightBtn)
+        self.navigationItem.rightBarButtonItem = barButtonItem
 
+     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
