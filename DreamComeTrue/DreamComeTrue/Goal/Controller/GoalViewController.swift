bca4cefd9580f606f92757395c1b9ba650dd7cf4
shuizheng <m15734107984@163.com>
2016-10-12 10:26:20 +0800

    change   goalViewControll’s UI

--- DreamComeTrue/DreamComeTrue/Goal/Controller/GoalViewController.swift
+++ DreamComeTrue/DreamComeTrue/Goal/Controller/GoalViewController.swift
@@ -2,74 +2,20 @@
 //  GoalViewController.swift
 //  DreamComeTrue
 //
-//  Created by 水蒸气很快乐 on 16/10/11.
+//  Created by 水蒸气很快乐 on 16/10/12.
 //  Copyright © 2016年 水蒸气很快乐. All rights reserved.
 //
 
 import UIKit
 
-class GoalViewController: UIViewController
-{
-    
-    //var  mainScrollView : UIScrollView?
+class GoalViewController: UIViewController {
 
-    
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        setupRightBarButtonItem()
-       
-    }
-    //创建导航栏右侧按钮
-     func setupRightBarButtonItem()
-     {
-        let rightBtn: UIButton = UIButton(type: .custom)
-        rightBtn.frame = CGRect(x: 0,y: 0,width: 40,height:25)
-        rightBtn.setTitle("编辑", for: UIControlState())
-        rightBtn.titleLabel?.font = UIFont.systemFont(ofSize: 13)
-        rightBtn.setTitleColor(UIColor.white, for: UIControlState())
-        rightBtn.layer.borderColor = UIColor.white.cgColor
-        rightBtn.layer.borderWidth = 1
-        rightBtn.layer.cornerRadius = 3
-        let barButtonItem = UIBarButtonItem(customView:rightBtn)
-        self.navigationItem.rightBarButtonItem = barButtonItem
 
-     }
-    //创建目标按钮
-     func setupGoalButton()
-     {
-        let FGBtn:UIButton = UIButton(type: .custom)
-        FGBtn.frame = CGRect(x: 0,y: 0,width:kMAIN_SIZE.width/4,height:kMAIN_SIZE.height/20)
-        FGBtn.setTitle("上一目标", for: UIControlState())
-        FGBtn.titleLabel?.font = UIFont.systemFont(ofSize: 9)
-        FGBtn.setTitleColor(UIColor.white, for: UIControlState())
-        FGBtn.backgroundColor = UIColor.red
-        FGBtn.layer.borderWidth = 0.5
-        FGBtn.layer.cornerRadius = 2
-        let SGBtn:UIButton = UIButton(type: .custom)
-        SGBtn.frame = CGRect(x: 0,y: 0,width: 40,height:25)
-        SGBtn.setTitle("目标一", for: UIControlState())
-        SGBtn.titleLabel?.font = UIFont.systemFont(ofSize: 12)
-        SGBtn.setTitleColor(UIColor.white, for: UIControlState())
-        SGBtn.layer.borderColor = UIColor.white.cgColor
-        SGBtn.layer.borderWidth = 1
-        SGBtn.layer.cornerRadius = 3
-        let TGBtn:UIButton = UIButton(type: .custom)
-        TGBtn.frame = CGRect(x: 0,y: 0,width: 40,height:25)
-        TGBtn.setTitle("下一目标", for: UIControlState())
-        TGBtn.titleLabel?.font = UIFont.systemFont(ofSize:9)
-        TGBtn.setTitleColor(UIColor.white, for: UIControlState())
-        TGBtn.layer.borderColor = UIColor.white.cgColor
-        TGBtn.layer.borderWidth = 1
-        TGBtn.layer.cornerRadius = 3
-        self.view.addSubview(FGBtn)
-        
+        // Do any additional setup after loading the view.
     }
-    //创建全局滚动视图
-    func setupMainScrollView()
-    {
-    
-    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
