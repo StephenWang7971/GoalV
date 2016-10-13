507713d1656826fb0fe6006161f8d11b68fb113c
shuizheng <m15734107984@163.com>
2016-10-13 18:06:24 +0800

    change     NSArray’s   database  and    transform   coredata’s database

--- DreamComeTrue/DreamComeTrue/Home/Controller/CheckViewController.swift
+++ DreamComeTrue/DreamComeTrue/Home/Controller/CheckViewController.swift
@@ -13,17 +13,21 @@ class CheckViewController: UIViewController,UITableViewDelegate,UITableViewDataS
 {
     var tableView : UITableView?
     var items :NSMutableArray?
-    var  account = ""
-    var age:String?
-    var finishtime:String?
-    var starttime:String?
-    
+    //var ageAry :NSMutableArray?
+    var ageAry:[AnyObject] = []
+    var finishtimeAry:[AnyObject] = []
+    var starttimeAry :[AnyObject] = []
+    var accountAry:[AnyObject] = []
+    var account :AnyObject!
+    var age :AnyObject!
+    var finishtime:AnyObject!
+    var starttime :AnyObject!
+    var  number: AnyObject!
     override func viewDidLoad()
     {
         super.viewDidLoad()
         self.title = "储蓄历史"
         self.navigationController?.navigationBar.titleTextAttributes =  [NSForegroundColorAttributeName: UIColor.white]
-        self.items = NSMutableArray()
         getPerson()
         setupTableView()
     }
@@ -38,7 +42,7 @@ class CheckViewController: UIViewController,UITableViewDelegate,UITableViewDataS
     //tableView的代理方法
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-         return self.items!.count
+         return number as! Int
     }
     func getContext () -> NSManagedObjectContext
     {
@@ -48,21 +52,24 @@ class CheckViewController: UIViewController,UITableViewDelegate,UITableViewDataS
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-       var showCell: CheckTableViewCell! = self.tableView!.dequeueReusableCell(withIdentifier: "cell") as? CheckTableViewCell
+      //var showCell: CheckTableViewCell! = self.tableView!.dequeueReusableCell(withIdentifier: "cell", forIndexPath:indexPath) as? CheckTableViewCell
+       var showCell: CheckTableViewCell!  = self.tableView!.dequeueReusableCell(withIdentifier: "cell", for: indexPath)as?CheckTableViewCell
+        
         if showCell == nil
         {
             showCell = self.tableView!.dequeueReusableCell(withIdentifier: "cell") as? CheckTableViewCell
 
         }
         showCell = self.tableView?.dequeueReusableCell(withIdentifier: "cell")as?CheckTableViewCell
-        showCell.DateLabel.text = self.starttime
-        //let selfaccount = String(describing: Int (self.account!))
-       // print("------ \(String(describing: Int(self.account!)))")
-     //   print("HELLO OF \(selfaccount)")
-        showCell.TimeLabel.text = self.account
-       //showCell.TimeLabel.text = "111111"
-        showCell.GoalLabel.text = self.finishtime
-        showCell.MoneyLabel.text = self.age
+        let myData = SaveDataBase()
+        myData.Maccount = self.accountAry[indexPath.row] as? String
+        print("HEll0----",self.accountAry.count)
+        showCell.MoneyLabel.text = "11111111"
+
+//        print(self.starttime)
+//    
+       showCell.GoalLabel.text = self.age as! String?
+        showCell.MoneyLabel.text = self.finishtime as! String?
         return showCell
        }
        //获取 coreDate  存储的数据
@@ -70,32 +77,25 @@ class CheckViewController: UIViewController,UITableViewDelegate,UITableViewDataS
     {
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Goal")
         do {
-            let searchResults = try getContext().fetch(fetchRequest)
+            let   searchResults = try getContext().fetch(fetchRequest)
+             self.items?.addObjects(from: searchResults)
             print("numbers of \(searchResults.count)")
-            self.items?.addObjects(from:searchResults)
-              print(" \(self.items?.count)")
+            number = Int(searchResults.count) as AnyObject?
+            print(" \(self.items?.count)")
             for p in (searchResults as! [NSManagedObject])
             {
                 print("account:  \(p.value(forKey: "account")!) finishtime: \(p.value(forKey: "finishtime")!)name:  \(p.value(forKey: "name")!) starttime: \(p.value(forKey: "starttime")!)")
+                self.account = p.value(forKey:"account") as AnyObject!
+                self.age = p.value(forKey: "finishtime")as AnyObject!
+                self.finishtime = p.value(forKey: "name")as AnyObject!
+                self.starttime = p.value(forKey: "starttime")as AnyObject!
+                self.ageAry.append(self.age)
+                self.accountAry.append(self.account)
+                self.starttimeAry.append(self.starttime)
+                self.finishtimeAry.append(self.finishtime)
               
-                //self.account = p.value(forKey:"account")! as! String
-                print(self.account)
-                print(p.value(forKey:"account")!)
-                
-                let   myTest = p.value(forKey:"account")!
-                print(myTest)
-                self.account = myTest as! String
-
-                
-                print(self.account)
-                //self.account = p.value(forKey:"account") as!String?
-//                print(String(p.value(forKey:"account")),
-////                String(String(self.account!))
-////                print("---------------\((self.account!))")
-//                self.finishtime = "\(p.value(forKey:"finishtime"))"
-//                self.age = "\(p.value(forKey:"name"))"
-//                self.starttime = "\(p.value(forKey:"starttime"))"
             }
+              print("----------------------------------------------------", self.accountAry.count)
         } catch
         {
             print(error)
---
+++ DreamComeTrue/DreamComeTrue/Home/Controller/SaveDataBase.swift
@@ -0,0 +1,17 @@
+//
+//  SaveDataBase.swift
+//  DreamComeTrue
+//
+//  Created by 水蒸气很快乐 on 16/10/13.
+//  Copyright © 2016年 水蒸气很快乐. All rights reserved.
+//
+
+import UIKit
+
+class SaveDataBase: NSObject {
+    var Maccount :String?
+    var Mage :String?
+    var Mfinishtime:String?
+    var Mstarttime :String?
+
+}
